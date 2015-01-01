a0ce628a747f28d68c9c79587aae69e8b457e038
Vignesh Nandha Kumar <viky.nandha@gmail.com>
2015-01-01 13:02:21 +0530

    Setup gulp build

---
+++ gulpfile.js
@@ -0,0 +1,66 @@
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    less = require('gulp-less'),
+    path = require('path'),
+
+    src = {
+        html: ['index.html'],
+        images: ['images/*'],
+        css: ['stylesheets/less/style.less'],
+        fonts: ['fonts/**/*'],
+        js: ['js/script.js']
+    },
+
+    dest = {
+        html: 'build/',
+        images: 'build/images/',
+        css: 'build/stylesheets/css/',
+        fonts: 'build/fonts/',
+        js: 'build/js/'
+    };
+
+
+gulp.task('html', function () {
+    gulp.src(src.html)
+        .pipe(gulp.dest(dest.html));
+});
+
+
+gulp.task('images', function () {
+    gulp.src(src.images)
+        .pipe(gulp.dest(dest.images));
+});
+
+
+gulp.task('css', function () {
+    gulp.src(src.css)
+        .pipe(less({
+            compress: true
+        }))
+        .pipe(gulp.dest(dest.css));
+});
+
+
+gulp.task('fonts', function () {
+    gulp.src(src.fonts)
+        .pipe(gulp.dest(dest.fonts));
+});
+
+
+gulp.task('js', function () {
+    gulp.src(src.js)
+        .pipe(gulp.dest(dest.js));
+});
+
+
+gulp.task('watch', function() {
+    gulp.watch(src.html, ['html']);
+    gulp.watch(src.images, ['images']);
+    gulp.watch(src.css, ['css']);
+    gulp.watch(src.fonts, ['fonts']);
+    gulp.watch(src.js, ['js']);
+});
+
+
+gulp.task('default', ['html', 'images', 'css', 'fonts', 'js', 'watch'], function () {
+});
